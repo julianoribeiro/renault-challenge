287c0c97127758f33c98660bfe2ca46d98e75690
theo hillmann Luz Coelho <70286183+theohillmann@users.noreply.github.com>
2024-09-20 09:27:25 -0300

    feat: add map to main page

--- interface/main.py
+++ interface/main.py
@@ -1,9 +1,46 @@
+import os
 import pandas as pd
 import streamlit as st
 
 
+def get_csv_root(file_name):
+    file_path = os.path.abspath(__file__)
+    parent_directory = os.path.dirname(file_path)
+    return os.path.join(os.path.dirname(parent_directory), file_name)
+
+
+def get_materials_by_supplier(address_df, suppliers_df):
+    return_dict = {}
+    suppliers = address_df["Fornecedor"]
+    for supplier in suppliers:
+        supplier_row = suppliers_df[suppliers_df["Fornecedor"] == supplier].iloc[0]
+        supplier_materials_list = supplier_row[supplier_row == "X"].index.tolist()
+        return_dict[supplier] = supplier_materials_list
+
+    return return_dict
+
+
+def get_volume_by_supplier(material_df, address_df, suppliers_df):
+    total_volume_dict = {}
+    suppliers_materials_dict = get_materials_by_supplier(address_df, suppliers_df)
+    for supplier, list_of_materials in suppliers_materials_dict.items():
+        volume_list = [
+            float(material_df[material_df["Material"] == x]["Quantidade_ton"])
+            for x in list_of_materials
+        ]
+        total_volume = sum(volume_list)
+        total_volume_dict[supplier] = total_volume
+
+    return total_volume_dict
+
+
 def main():
-    pass
+    address_df = pd.read_csv("interface/address_suppliers.csv", sep=";")
+    suppliers_df = pd.read_csv(get_csv_root("suppliers.csv"), sep=";")
+    materials_df = pd.read_csv(get_csv_root("materials.csv"), sep=";")
+    volumes_dict = get_volume_by_supplier(materials_df, address_df, suppliers_df)
+    address_df["volume"] = address_df["Fornecedor"].map(volumes_dict)
+    st.map(address_df, longitude="longitude", latitude="latitude", size="volume")
 
 
 if __name__ == "__main__":
--- interface/pages/edit csv.py
+++ interface/pages/edit csv.py
@@ -3,6 +3,8 @@ import pandas as pd
 import streamlit as st
 from dataclasses import dataclass, field
 from dotenv import load_dotenv
+import plotly.express as px
+
 
 load_dotenv()
 
@@ -36,6 +38,7 @@ CSV_DICT = {
 @dataclass
 class EditCsvPage:
     edited_df: pd.DataFrame = field(init=False)
+    current_csv: str = field(init=False)
 
     def __post_init__(self):
         self.show_page()
@@ -43,8 +46,10 @@ class EditCsvPage:
     def show_page(self):
         csv_file = self.select_csv()
         if csv_file is not None:
+            self.current_csv = csv_file
             df = pd.read_csv(csv_file, sep=";")
             self.columns_layout(df, csv_file)
+            self.graphs()
         else:
             st.write("Please select a CSV file to edit.")
 
@@ -76,6 +81,110 @@ class EditCsvPage:
                 st.success("Arquivo salvo com sucesso!")
             else:
                 st.error("No changes to save.")
+
+    def graphs(self):
+        current_file_name = os.path.basename(self.current_csv).split(".")[0]
+        df = pd.read_csv(self.current_csv, sep=";")
+        if current_file_name == "costs":
+            self.costs_graphs(df)
+
+        if current_file_name == "materials":
+            self.material_graph(df)
+
+        if current_file_name == "suppliers":
+            self.suppliers_graph(df)
+
+        if current_file_name == "vehicles":
+            self.vehicles_graph(df)
+
+    def costs_graphs(self, df):
+        col1, col2 = st.columns(2)
+        with col1:
+            cols_to_plot = ["Km", "Van", "Leve", "Toco", "Truck", "Bitrem", "Fiorino"]
+            df_plot = df[cols_to_plot]
+            fig = px.line(
+                df_plot,
+                x="Km",
+                y=cols_to_plot[1:],
+                labels={"value": "Custo", "Km": "Quilometragem (Km)"},
+                title="Variação de Custo por Quilometragem para Diferentes Veículos",
+            )
+            fig.update_layout(
+                xaxis_title="Quilometragem (Km)",
+                yaxis_title="Custo",
+                legend_title_text="Veículos",
+                hovermode="x unified",
+            )
+
+            st.plotly_chart(fig)
+
+        with col2:
+            km_selecionado = st.selectbox(
+                "Selecione a Quilometragem", options=df["Km"].unique()
+            )
+            dados_filtrados = df[df["Km"] == km_selecionado]
+            dados_filtrados = dados_filtrados.drop("Km", axis=1)
+            dados_para_grafico = dados_filtrados.melt(
+                var_name="Veículo", value_name="Custo"
+            )
+            fig = px.bar(
+                dados_para_grafico,
+                x="Veículo",
+                y="Custo",
+                title=f"Comparação de Custos para Quilometragem {km_selecionado}",
+                labels={"Custo": "Custo", "Veículo": "Tipo de Veículo"},
+            )
+            fig.update_layout(
+                xaxis_title="Tipo de Veículo", yaxis_title="Custo", xaxis_tickangle=-45
+            )
+            st.plotly_chart(fig)
+
+    def material_graph(self, df):
+        unit_selected = st.selectbox(
+            "Selecione a unidade", options=["Quantidade_m3", "Quantidade_ton"]
+        )
+        fig = px.bar(
+            df,
+            x="Material",
+            y=unit_selected,
+        )
+        st.plotly_chart(fig)
+
+    def suppliers_graph(self, df):
+        distance_data_numeric = df[
+            ["Fornecedor1", "Fornecedor2", "Fornecedor3", "Fornecedor4", "Fornecedor5"]
+        ].apply(pd.to_numeric)
+
+        fig = px.imshow(
+            distance_data_numeric.values,
+            labels=dict(x="Para", y="De", color="Distância"),
+            x=[
+                "Fornecedor1",
+                "Fornecedor2",
+                "Fornecedor3",
+                "Fornecedor4",
+                "Fornecedor5",
+            ],
+            y=df["Fornecedor"],
+            color_continuous_scale="Viridis",
+            text_auto=True,
+        )
+
+        fig.update_layout(
+            title="Mapa de Calor das Distâncias entre Fornecedores",
+            xaxis_title="Para",
+            yaxis_title="De",
+        )
+
+        st.plotly_chart(fig)
+
+    def vehicles_graph(self, df):
+        fig = px.bar(
+            df,
+            y="Quantidade",
+            x="Veiculo",
+        )
+        st.plotly_chart(fig)
 
 
 def main():
